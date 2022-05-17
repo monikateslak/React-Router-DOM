11b98236588cb6ae788a2627eb96e49943448daa
Monika Mona <mona.mt.89@gmail.com>
2022-05-17 08:06:10 +0200

    Images component test, hook useState, useEffect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 //import { useState, useEffect, useReducer } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Images from './Images'
 //import { Link } from 'react-router-dom'
 import Navigation from './components/nav'
+
 import About from './About'
 import Footer from './Footer'
 
 const Home = () => <Home></Home>
+const One = () => <div>One Container</div>
+const Two = () => <div>Two Container</div>
 
 function App() {
   //const [enabled, toggle] = useReducer((x) => !x, false);
@@ -18,11 +22,14 @@ function App() {
       <div className="App">
         <div className="App-header">
           <Home></Home>
+          <Images></Images>
           <Navigation></Navigation>
           <About></About>
           <Footer></Footer>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/one" element={<One />}></Route>
+            <Route path="two" element={<Two></Two>}></Route>
             <Route path="/nav" element={<Navigation />}></Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/footer" element={<Footer />}></Route>
--- src/Images.js
+++ src/Images.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 function Images() {
+  useEffect(() => {
+    GetItems()
+  }, [])
+  const [items, setItems] = useState([])
+  const GetItems = async () => {
+    const Response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos/',
+    ).then((Response) => Response.json())
+
+    setItems(Response)
+
+    useEffect(() => {
+      GetItems()
+    }, [])
+  }
+
   return (
     <div className="images-container">
+      {items && items.map((item) =>)}
       <Router>
         <div className="one">
-          <Link to="/one">One Container</Link>
+          <Link to="/one">One</Link>
         </div>
         <div className="two">
-          <Link to="/two"> Two Container</Link>
+          <Link to="/two"> Two</Link>
         </div>
         <div className="three">
-          <Link to="/three">Three Container </Link>
+          <Link to="/three">Three</Link>
         </div>
       </Router>
     </div>
